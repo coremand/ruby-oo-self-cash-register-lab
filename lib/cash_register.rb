d540f1e61a545421aecd6b80a0a31bbd39f2eb6d
coremand <valinevite@gmail.com>
2020-12-15 16:45:48 -0700

    commit tree 3b307bc099155d57932edfc5bd2bd5de7d8e9b99

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+require "pry"
 
+class CashRegister
+    attr_accessor :discount
+    attr_accessor :total
+    def initialize(discount=0)
+        @discount = discount
+        @total = 0
+        @items = []
+    end
+
+    def add_item(title,price,quantity=1)
+        @quantity = quantity
+        @items << ([title] * @quantity)
+        self.total += price * quantity
+    end
+
+    def apply_discount
+        if @discount != 0
+            self.total = @total - (@total  * @discount/100)
+            return "After the discount, the total comes to $#{@total}."
+        else
+           "There is no discount to apply."
+        end
+    end
+
+    def items
+        @items.flatten
+        
+    end
+end
